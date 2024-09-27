a6223f83e81cb91ff4bdcb3228c0871a0eabd1d3
nhuamani <nhuamani.c7@gmail.com>
2024-09-27 10:04:04 -0500

    first commit

---
+++ src/test/java/com/nhuamani/bankAppApi/BankAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhuamani.bankAppApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
